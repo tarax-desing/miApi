e73bd635b4c6b7fbfcaf1e87ae2b3376d9bbedaa
tarax-desing <histor.iade0a100@gmail.com>
2024-09-29 08:22:02 +0200

    dom-menuBurguer

--- javaScript.js
+++ javaScript.js
@@ -7,3 +7,15 @@ buttons.forEach(button => {
         console.log(`Botón ${button.textContent} clickeado`);
     });
 });
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
